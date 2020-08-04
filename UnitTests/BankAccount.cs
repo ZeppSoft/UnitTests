facfe311384c7bc9c95f39082168f75211252408
Viktor Shershnov <viktor.shershnov@quipu.de>
2020-08-04 14:42:51 +0300

    Add project files.

---
+++ UnitTests/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    public class BankAccount
+    {
+        public int Balance { get; private set; }
+
+        public BankAccount(int startingBalance)
+        {
+            Balance = startingBalance;
+        }
+
+        public void Deposit(int amount)
+        {
+
+        }
+        public void Withdraw(int amount)
+        {
+
+        }
+    }
+}
---
+++ UnitTests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class BankAccountTest
+    {
+        [Test]
+        public void BankAccountShouldIncreaseOnPositiveDeposit()
+        {
+            Assert.Fail();
+        }
+    }
+}
