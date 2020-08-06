c63e6ae8bfc58b5f022a020c823c7bda5ac5be1d
Viktor Shershnov <viktor.shershnov@quipu.de>
2020-08-06 12:15:46 +0300

    Moq final

--- UnitTests/BankAccount4UnitTests.cs
+++ UnitTests/BankAccount4UnitTests.cs
@@ -8,141 +8,168 @@ using NUnit.Framework;
 
 namespace UnitTests
 {
+    public interface IFoo
+    {
+        bool DoSomething(string str);
+
+    }
     public interface ILog
     {
         bool Write(string msg);
     }
 
-    public class ConsoleLog : ILog
-    {
-        public bool Write(string msg)
-        {
-            Console.WriteLine(msg);
-            return true;
-        }
-    }
-    public class NullLog : ILog
-    {
-        public bool Write(string msg)
-        {
-            return true;
-        }
-    }
-
-    public class Null<T> : DynamicObject where T : class
-    {
-        public static T Instance => new Null<T>().ActLike<T>();
-
-        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
-        {
-            result = Activator.CreateInstance(typeof(T).GetMethod(binder.Name).ReturnType);
-            return true;
-        }
-    }
-
-    public class NullLogWithResult : ILog
-    {
-        private bool expectedResult;
-
-        public NullLogWithResult(bool expectedResult)
-        {
-            this.expectedResult = expectedResult;
-        }
-
-        public bool Write(string msg)
-        {
-            return expectedResult;
-        }
-    }
-
-    public class LogMock : ILog
-    {
-        private bool expectedResult;
-        public Dictionary<string, int> MethodCallCount;
-
-        public LogMock(bool expectedResult)
-        {
-            this.expectedResult = expectedResult;
-            MethodCallCount = new Dictionary<string, int>();
-        }
-
-        private void AddOrIncrement(string methodName)
-        {
-            if (MethodCallCount.ContainsKey(methodName))
-                MethodCallCount[methodName]++;
-            else
-                MethodCallCount.Add(methodName,1);
-        }
-
-        public bool Write(string msg)
-        {
-            AddOrIncrement(nameof(Write));
-            return expectedResult; 
-        }
-    }
-
     public class BankAccount
     {
-        private readonly ILog log;
-        public int Balance { get;  set; }
+        public  int Balance { set; get; }
+        private ILog log;
 
         public BankAccount(ILog log)
         {
             this.log = log;
         }
-        public void Deposit(int amount)
-        {
-            //if (amount < 0)
-            //    throw new ArgumentException("Deposit amounts should be positive", nameof(amount));
-            log.Write($"Depositing {amount}");
-
-            Balance += amount;
-        }
-        public bool Withdraw(int amount)
-        {
-            if (Balance >= amount)
-            {
-                Balance -= amount;
-                return true;
-            }
-            else
-                return false;
-        }
-    }
-   
-    public class BankAccount4UnitTests
-    {
-        private readonly ILog log;
-        public int Balance { get; private set; }
-
-        public BankAccount4UnitTests(int startingBalance)
-        {
-            Balance = startingBalance;
-        }
-
-        public BankAccount4UnitTests(ILog log)
-        {
-            this.log = log;
-        }
 
         public void Deposit(int amount)
         {
-            if (amount < 0)
-                throw  new ArgumentException("Deposit amounts should be positive", nameof(amount));
-
+            log.Write($"User was deposited {amount}");
             Balance += amount;
-        }
-        public bool Withdraw(int amount)
-        {
-            if (Balance >= amount)
-            {
-                Balance -= amount;
-                return true;
-            }
-            else
-                return false;
         }
     }
 
-   
+    //public interface ILog
+    //{
+    //    bool Write(string msg);
+    //}
+
+    //public class ConsoleLog : ILog
+    //{
+    //    public bool Write(string msg)
+    //    {
+    //        Console.WriteLine(msg);
+    //        return true;
+    //    }
+    //}
+    //public class NullLog : ILog
+    //{
+    //    public bool Write(string msg)
+    //    {
+    //        return true;
+    //    }
+    //}
+
+    //public class Null<T> : DynamicObject where T : class
+    //{
+    //    public static T Instance => new Null<T>().ActLike<T>();
+
+    //    public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+    //    {
+    //        result = Activator.CreateInstance(typeof(T).GetMethod(binder.Name).ReturnType);
+    //        return true;
+    //    }
+    //}
+
+    //public class NullLogWithResult : ILog
+    //{
+    //    private bool expectedResult;
+
+    //    public NullLogWithResult(bool expectedResult)
+    //    {
+    //        this.expectedResult = expectedResult;
+    //    }
+
+    //    public bool Write(string msg)
+    //    {
+    //        return expectedResult;
+    //    }
+    //}
+
+    //public class LogMock : ILog
+    //{
+    //    private bool expectedResult;
+    //    public Dictionary<string, int> MethodCallCount;
+
+    //    public LogMock(bool expectedResult)
+    //    {
+    //        this.expectedResult = expectedResult;
+    //        MethodCallCount = new Dictionary<string, int>();
+    //    }
+
+    //    private void AddOrIncrement(string methodName)
+    //    {
+    //        if (MethodCallCount.ContainsKey(methodName))
+    //            MethodCallCount[methodName]++;
+    //        else
+    //            MethodCallCount.Add(methodName,1);
+    //    }
+
+    //    public bool Write(string msg)
+    //    {
+    //        AddOrIncrement(nameof(Write));
+    //        return expectedResult; 
+    //    }
+    //}
+
+    //public class BankAccount
+    //{
+    //    private readonly ILog log;
+    //    public int Balance { get;  set; }
+
+    //    public BankAccount(ILog log)
+    //    {
+    //        this.log = log;
+    //    }
+    //    public void Deposit(int amount)
+    //    {
+    //        //if (amount < 0)
+    //        //    throw new ArgumentException("Deposit amounts should be positive", nameof(amount));
+    //        log.Write($"Depositing {amount}");
+
+    //        Balance += amount;
+    //    }
+    //    public bool Withdraw(int amount)
+    //    {
+    //        if (Balance >= amount)
+    //        {
+    //            Balance -= amount;
+    //            return true;
+    //        }
+    //        else
+    //            return false;
+    //    }
+    //}
+
+    //public class BankAccount4UnitTests
+    //{
+    //    private readonly ILog log;
+    //    public int Balance { get; private set; }
+
+    //    public BankAccount4UnitTests(int startingBalance)
+    //    {
+    //        Balance = startingBalance;
+    //    }
+
+    //    public BankAccount4UnitTests(ILog log)
+    //    {
+    //        this.log = log;
+    //    }
+
+    //    public void Deposit(int amount)
+    //    {
+    //        if (amount < 0)
+    //            throw  new ArgumentException("Deposit amounts should be positive", nameof(amount));
+
+    //        Balance += amount;
+    //    }
+    //    public bool Withdraw(int amount)
+    //    {
+    //        if (Balance >= amount)
+    //        {
+    //            Balance -= amount;
+    //            return true;
+    //        }
+    //        else
+    //            return false;
+    //    }
+    //}
+
+
 }
--- UnitTests/Class1.cs
+++ UnitTests/Class1.cs
@@ -2,147 +2,185 @@
 using System;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
+using Moq;
 
 namespace UnitTests
 {
-    [TestFixture]
-    public class BankAccountUnitTests
-    {
-        private BankAccount4UnitTests ba;
-
-        [SetUp]
-        public void Setup()
-        {
-            ba = new BankAccount4UnitTests(100);
-        }
-        [Test]
-        public void BankAccountShouldIncreaseOnPositiveDeposit()
-        {
-            //AAA(arrange+act+assert) approach
-
-            //arrange (init)
-            //var ba = new BankAccount4UnitTests(100);
-
-            //act
-            ba.Deposit(100);
-
-            //assert
-            Assert.That(ba.Balance,Is.EqualTo(200));
-
-            //Assert.That(2 + 2, Is.EqualTo(5));
-            //Warn.If(2 + 3, Is.Not.EqualTo(6));
-            //Warn.If(() => 2 + 2, Is.Not.EqualTo(5).After(2000));
-        }
-        [Test]
-        public void TryingToWithdrawal()
-        {
-            ba.Withdraw(100);
-             
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(ba.Balance, Is.EqualTo(0));
-                Assert.That(ba.Balance, Is.LessThan(1));
-            });
-        }
-        [Test]
-        public void BankAccountShouldThrowOnNonPositiveAmount()
-        {
-           var ex = Assert.Throws<ArgumentException>(()=>
-            ba.Deposit(-1)
-            );
-
-           StringAssert.StartsWith("Deposit amounts should be positive",ex.Message);
-        }
-
-       
-
-        //public void DepositIntegrationTest()
-        //{
-
-        //}
-    }
-
     [TestFixture]
     public class BankAccountTests
     {
         private BankAccount ba;
         [Test]
-        public void DepositIntegrationTest()
+        public void DepositTest()
         {
-            ba = new BankAccount(new ConsoleLog()){Balance =  100};
+            var log = new Mock<ILog>();
+            ba = new BankAccount(log.Object){Balance =  100};
             ba.Deposit(100);
+
             Assert.That(ba.Balance,Is.EqualTo(200));
         }
-        [Test]
-        public void DepositIntegratedTestWithFake()
-        {
-            var log = new NullLog();
-            ba = new BankAccount(log){Balance =  100};
-            ba.Deposit(100);
-            Assert.That(ba.Balance, Is.EqualTo(200));
-
-        }
-        [Test]
-        public void DepositIntegratedTestWithFakeNull()
-        {
-            var log = Null<ILog>.Instance;
-            ba = new BankAccount(log) { Balance = 100 };
-            ba.Deposit(100);
-            Assert.That(ba.Balance, Is.EqualTo(200));
-
-        }
-
-        [Test]
-        public void DepositIntegratedTestWithFakeNullStub()
-        {
-            var log = new NullLogWithResult(true);
-            ba = new BankAccount(log) { Balance = 100 };
-            ba.Deposit(100);
-            Assert.That(ba.Balance, Is.EqualTo(200));
-        }
-
-        [Test]
-        public void DepositTestWithMock()
-        {
-            var log = new LogMock(true);
-            ba = new BankAccount(log){Balance =  100};
-            ba.Deposit(100);
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(ba.Balance,Is.EqualTo(200));
-                Assert.That(log.MethodCallCount[nameof(LogMock.Write)],Is.EqualTo(1));
-            });
-
-        }
 
     }
-
-
     [TestFixture]
-    public class DataDrivenTest
+    public class FooTests
     {
-        private BankAccount4UnitTests ba;
-        [SetUp]
-        public void SetUp()
-        {
-            ba = new BankAccount4UnitTests(100);
-        }
         [Test]
-        [TestCase(50,true,50)]
-        [TestCase(100,true,0)]
-        [TestCase(1000,false,100)]
-        public void TestMultipleWithdrawalScenarios(int amountToWithdraw, bool shouldSucceed, int expectedBalance)
+        public void DoSomethingTest()
         {
-            var result = ba.Withdraw(amountToWithdraw);
-            //Warn.If(!result,"Failed for some reason");
+            var mock = new Mock<IFoo>();
+            mock.Setup(foo => foo.DoSomething("ping")).Returns(true);
+            //mock.Setup(foo => foo.DoSomething("pong")).Returns(false);
+            mock.Setup(foo => foo.DoSomething(It.IsIn("pong", "foo"))).Returns(false);
 
             Assert.Multiple(() =>
             {
-                Assert.That(result,Is.EqualTo(shouldSucceed));
-                Assert.That(ba.Balance,Is.EqualTo(expectedBalance));
+                Assert.IsTrue(mock.Object.DoSomething("ping"));
+                Assert.IsFalse(mock.Object.DoSomething("foo"));
+
             });
         }
+
     }
+
+
+    //[TestFixture]
+    //public class BankAccountUnitTests
+    //{
+    //    private BankAccount4UnitTests ba;
+
+    //    [SetUp]
+    //    public void Setup()
+    //    {
+    //        ba = new BankAccount4UnitTests(100);
+    //    }
+    //    [Test]
+    //    public void BankAccountShouldIncreaseOnPositiveDeposit()
+    //    {
+    //        //AAA(arrange+act+assert) approach
+
+    //        //arrange (init)
+    //        //var ba = new BankAccount4UnitTests(100);
+
+    //        //act
+    //        ba.Deposit(100);
+
+    //        //assert
+    //        Assert.That(ba.Balance,Is.EqualTo(200));
+
+    //        //Assert.That(2 + 2, Is.EqualTo(5));
+    //        //Warn.If(2 + 3, Is.Not.EqualTo(6));
+    //        //Warn.If(() => 2 + 2, Is.Not.EqualTo(5).After(2000));
+    //    }
+    //    [Test]
+    //    public void TryingToWithdrawal()
+    //    {
+    //        ba.Withdraw(100);
+             
+
+    //        Assert.Multiple(() =>
+    //        {
+    //            Assert.That(ba.Balance, Is.EqualTo(0));
+    //            Assert.That(ba.Balance, Is.LessThan(1));
+    //        });
+    //    }
+    //    [Test]
+    //    public void BankAccountShouldThrowOnNonPositiveAmount()
+    //    {
+    //       var ex = Assert.Throws<ArgumentException>(()=>
+    //        ba.Deposit(-1)
+    //        );
+
+    //       StringAssert.StartsWith("Deposit amounts should be positive",ex.Message);
+    //    }
+
+       
+
+    //    //public void DepositIntegrationTest()
+    //    //{
+
+    //    //}
+    //}
+
+    //[TestFixture]
+    //public class BankAccountTests
+    //{
+    //    private BankAccount ba;
+    //    [Test]
+    //    public void DepositIntegrationTest()
+    //    {
+    //        ba = new BankAccount(new ConsoleLog()){Balance =  100};
+    //        ba.Deposit(100);
+    //        Assert.That(ba.Balance,Is.EqualTo(200));
+    //    }
+    //    [Test]
+    //    public void DepositIntegratedTestWithFake()
+    //    {
+    //        var log = new NullLog();
+    //        ba = new BankAccount(log){Balance =  100};
+    //        ba.Deposit(100);
+    //        Assert.That(ba.Balance, Is.EqualTo(200));
+
+    //    }
+    //    [Test]
+    //    public void DepositIntegratedTestWithFakeNull()
+    //    {
+    //        var log = Null<ILog>.Instance;
+    //        ba = new BankAccount(log) { Balance = 100 };
+    //        ba.Deposit(100);
+    //        Assert.That(ba.Balance, Is.EqualTo(200));
+
+    //    }
+
+    //    [Test]
+    //    public void DepositIntegratedTestWithFakeNullStub()
+    //    {
+    //        var log = new NullLogWithResult(true);
+    //        ba = new BankAccount(log) { Balance = 100 };
+    //        ba.Deposit(100);
+    //        Assert.That(ba.Balance, Is.EqualTo(200));
+    //    }
+
+    //    [Test]
+    //    public void DepositTestWithMock()
+    //    {
+    //        var log = new LogMock(true);
+    //        ba = new BankAccount(log){Balance =  100};
+    //        ba.Deposit(100);
+
+    //        Assert.Multiple(() =>
+    //        {
+    //            Assert.That(ba.Balance,Is.EqualTo(200));
+    //            Assert.That(log.MethodCallCount[nameof(LogMock.Write)],Is.EqualTo(1));
+    //        });
+
+    //    }
+
+    //}
+
+
+    //[TestFixture]
+    //public class DataDrivenTest
+    //{
+    //    private BankAccount4UnitTests ba;
+    //    [SetUp]
+    //    public void SetUp()
+    //    {
+    //        ba = new BankAccount4UnitTests(100);
+    //    }
+    //    [Test]
+    //    [TestCase(50,true,50)]
+    //    [TestCase(100,true,0)]
+    //    [TestCase(1000,false,100)]
+    //    public void TestMultipleWithdrawalScenarios(int amountToWithdraw, bool shouldSucceed, int expectedBalance)
+    //    {
+    //        var result = ba.Withdraw(amountToWithdraw);
+    //        //Warn.If(!result,"Failed for some reason");
+
+    //        Assert.Multiple(() =>
+    //        {
+    //            Assert.That(result,Is.EqualTo(shouldSucceed));
+    //            Assert.That(ba.Balance,Is.EqualTo(expectedBalance));
+    //        });
+    //    }
+    //}
 }
